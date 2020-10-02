a9beab6f224b75a8d5a8ce456b36d3f4da1db747
DreamakerDimas <dart.dimas@gmail.com>
2020-10-02 18:54:34 +0300

    init

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div>Hello</div>
+    </>
+  );
+}
+
+export default App;

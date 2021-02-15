4b2d36bd68a4bd5db7603a5316b4d9d5417cd62d
DreamakerDimas <dart.dimas@gmail.com>
2021-02-15 20:24:00 +0200

    rest getUser

--- client/src/api/rest/restController.js
+++ client/src/api/rest/restController.js
@@ -2,3 +2,4 @@ import http from '../interceptor';
 
 export const login = (data) => http.post('/api/auth/login', data);
 export const register = (data) => http.post('/api/auth/register', data);
+export const getUser = () => http.get('/api/users/getUser');

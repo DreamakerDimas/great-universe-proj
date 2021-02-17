ddc67ddc02f55374746fde695aee9b1b7df7f68b
DreamakerDimas <dart.dimas@gmail.com>
2021-02-17 17:19:39 +0200

    logout feature

--- client/src/components/Header/UserMenu.js
+++ client/src/components/Header/UserMenu.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import CONSTANTS from '../../lib/constants';
 
 const UserMenu = ({ userData, logout }) => {
+  const logoutHandler = () => {
+    logout();
+    window.localStorage.removeItem(CONSTANTS.ACCESS_TOKEN);
+  };
+
   return (
     <>
       <div>{userData.name}</div>
-      <button onClick={logout}>Выйти</button>
+      <button onClick={logoutHandler}>Выйти</button>
     </>
   );
 };

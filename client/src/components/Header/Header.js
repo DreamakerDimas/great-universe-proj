ce26943154c82cd3bdd10d1c686ce32288581256
DreamakerDimas <dart.dimas@gmail.com>
2021-02-18 17:21:20 +0200

    styles logo & Home

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -17,30 +17,25 @@ const Header = ({ getUser, userData, error, loading, logout }) => {
 
   return (
     <div className={styles.headerContainer}>
-      <div className={styles.headerLogo}>
-        <Link to="/">
-          <div className={styles.logoContainer}>
-            <img src={logo} alt="logo" />
-          </div>
-        </Link>
-      </div>
+      <Link to="/" className={styles.headerLogo}>
+        <div className={styles.logoContainer}>
+          <img src={logo} alt="logo" />
+        </div>
+      </Link>
 
       <ul className={styles.navContainer}>
-        <li className={styles.navButton}>
-          <Link to="/">
-            <span>Домой</span>
-          </Link>
-        </li>
         <li className={styles.navButton}>
           <Link to="/articles">
             <span>Статьи</span>
           </Link>
         </li>
+
         <li className={styles.navButton}>
           <Link to="/history">
             <span>История</span>
           </Link>
         </li>
+
         <li className={styles.navButton}>
           <Link to="/map">
             <span>Карта</span>

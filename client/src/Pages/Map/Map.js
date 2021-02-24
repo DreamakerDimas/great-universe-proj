d5ac58d5c952638ec8eacd823059b1ace62db585
DreamakerDimas <dart.dimas@gmail.com>
2021-02-24 15:43:30 +0200

    svg (v1.0), basic Map settings

--- client/src/Pages/Map/Map.js
+++ client/src/Pages/Map/Map.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { ReactSVG } from 'react-svg';
+import styles from './Map.module.sass';
+
+const svgPath = process.env.PUBLIC_URL + '/map/svg/main.svg';
 
 const Map = () => {
+  // set class for inner SVG elements
+  const setClassName = (node) => {
+    const className = node.getAttribute('class');
+    node.setAttribute('class', styles[className]);
+  };
+
+  const setIdName = (node) => {
+    const idName = node.getAttribute('id');
+    node.setAttribute('id', styles[idName]);
+  };
+
+  // SVG middleware
+  const beforeInjectionHandler = (svg) => {
+    svg.classList.add(styles.svg);
+    svg.childNodes.forEach((node) => {
+      if (node.nodeName !== '#text') {
+        setClassName(node);
+        setIdName(node);
+      }
+    });
+  };
+
   return (
     <>
       <div>Map</div>
+      <div className={styles.mapContainer}>
+        <ReactSVG
+          src={svgPath}
+          beforeInjection={beforeInjectionHandler}
+        ></ReactSVG>
+      </div>
     </>
   );
 };

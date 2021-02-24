ca7fa533b4dacf932617876f53bf113fd53120fe
DreamakerDimas <dart.dimas@gmail.com>
2021-02-24 21:04:39 +0200

    zones styles, img added

--- client/src/Pages/Map/Map.js
+++ client/src/Pages/Map/Map.js
@@ -1,40 +1,15 @@
 import React from 'react';
-import { ReactSVG } from 'react-svg';
+import SVGZones from './SVGZones/SVGZones';
 import styles from './Map.module.sass';
 
-const svgPath = process.env.PUBLIC_URL + '/map/svg/main.svg';
+const pngPath = process.env.PUBLIC_URL + '/map/png/Milky_Way_Longitude.png';
 
 const Map = () => {
-  // set class for inner SVG elements
-  const setClassName = (node) => {
-    const className = node.getAttribute('class');
-    node.setAttribute('class', styles[className]);
-  };
-
-  const setIdName = (node) => {
-    const idName = node.getAttribute('id');
-    node.setAttribute('id', styles[idName]);
-  };
-
-  // SVG middleware
-  const beforeInjectionHandler = (svg) => {
-    svg.classList.add(styles.svg);
-    svg.childNodes.forEach((node) => {
-      if (node.nodeName !== '#text') {
-        setClassName(node);
-        setIdName(node);
-      }
-    });
-  };
-
   return (
     <>
-      <div>Map</div>
       <div className={styles.mapContainer}>
-        <ReactSVG
-          src={svgPath}
-          beforeInjection={beforeInjectionHandler}
-        ></ReactSVG>
+        <img src={pngPath} />
+        <SVGZones />
       </div>
     </>
   );
---
+++ client/src/Pages/Map/SVGZones/SVGZones.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { ReactSVG } from 'react-svg';
+
+import styles from './SVGZones.module.sass';
+
+const svgPath = process.env.PUBLIC_URL + '/map/svg/main.svg';
+
+const SVGZones = () => {
+  // set class for inner SVG elements
+  const setClassName = (node) => {
+    const className = node.getAttribute('class');
+    node.setAttribute('class', styles[className]);
+  };
+
+  // set id for inner SVG elements && set name
+  const setIdAndName = (node) => {
+    const idName = node.getAttribute('id');
+    node.setAttribute('name', idName);
+    if (styles[idName] === undefined) return;
+    node.setAttribute('id', styles[idName]);
+  };
+
+  // SVG middleware
+  const beforeInjectionHandler = (svg) => {
+    svg.classList.add(styles.svg);
+    svg.childNodes.forEach((node) => {
+      if (node.nodeName !== '#text') {
+        setClassName(node);
+        setIdAndName(node);
+      }
+    });
+  };
+
+  return (
+    <ReactSVG
+      className={styles.container}
+      src={svgPath}
+      beforeInjection={beforeInjectionHandler}
+    ></ReactSVG>
+  );
+};
+
+export default SVGZones;
